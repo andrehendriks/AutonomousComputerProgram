39aa12ce7f06da42ce322311c5ce1b89817df8f8
cyberpilot39 <weze10bj@kpnmail.nl>
2015-10-18 10:06:14 +0200

    commit test 2

--- AutonomousComputerProgram/Missy.cs
+++ AutonomousComputerProgram/Missy.cs
@@ -26,12 +26,12 @@ namespace AutonomousComputerProgram
         {
             myBot.loadSettings();
             AIMLbot.Utils.AIMLLoader loader = new AIMLbot.Utils.AIMLLoader(myBot);
-            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"/App_Data/Graphmaster.dat"))
-                myBot.loadFromBinaryFile(AppDomain.CurrentDomain.BaseDirectory + @"/App_Data/Graphmaster.dat");
-            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"/App_Data/Missy.db"))
-                myBot.loadFromBinaryFile(AppDomain.CurrentDomain.BaseDirectory + @"/App_Data/Missy.db");
-            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"/reasoner-3.3.2/reasoner-3.3.2/bin/su.dat"))
-                myBot.loadFromBinaryFile(AppDomain.CurrentDomain.BaseDirectory + @"/reasoner-3.3.2/reasoner-3.3.2/bin/su.dat");
+           // if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"/App_Data/Graphmaster.dat"))
+           //     myBot.loadFromBinaryFile(AppDomain.CurrentDomain.BaseDirectory + @"/App_Data/Graphmaster.dat");
+           // if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"/App_Data/Missy.db"))
+           //     myBot.loadFromBinaryFile(AppDomain.CurrentDomain.BaseDirectory + @"/App_Data/Missy.db");
+           // if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"/reasoner-3.3.2/reasoner-3.3.2/bin/su.dat"))
+           //     myBot.loadFromBinaryFile(AppDomain.CurrentDomain.BaseDirectory + @"/reasoner-3.3.2/reasoner-3.3.2/bin/su.dat");
             myBot.isAcceptingUserInput = false;
             loader.loadAIML(myBot.PathToAIML);
             myBot.isAcceptingUserInput = true;
